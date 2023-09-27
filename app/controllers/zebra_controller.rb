1e26ecd771a86788dc7e241160ac3bd72e0fcc80
Brian Waddell <139478254+Brian-Waddell@users.noreply.github.com>
2023-09-27 02:19:04 +0000

    09/26 Commit

---
+++ app/controllers/zebra_controller.rb
@@ -0,0 +1,55 @@
+class  ZebraController < ApplicationController 
+
+
+  def lion 
+
+ render ({:template => "/game_template/home"})
+
+
+
+
+
+
+
+
+  def girffae 
+   
+    @attempts = []
+
+    2.times do 
+
+    @first_die = rand(1..6) 
+
+    @attempts.push(@first_die)
+    end 
+    render({:template => "/game_template/two_dice_six"})
+    end 
+
+    def dice 
+      @array = []
+      2.times do 
+       die = rand(1..10)
+       @array.push(die)
+      end
+      render({:template => "/game_template/two_dice_ten"})
+    end 
+
+    def bird 
+      @array = []
+      1.times do 
+     @random_number_generator = rand(1..20)
+     @array.push(@random_number_generator)
+      end 
+
+render({:template => "/game_template/one_die_twenty"})
+  end
+
+  def mouse 
+
+    @rolls = []
+    5.times do 
+      dice = rand(1..4)
+      @rolls.push(dice)
+    end 
+    render({:template => "/game_template/five_dice_four"})
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
 
+  get("/", {:controller => "zebra", :action => "lion"})
+
+  get("/dice/2/6", {:controller => "zebra", :action => "girffae"})
+
+  get("/dice/2/10", {:controller => "zebra", :action => "dice"})
+
+  get("/dice/1/20", {:controller => "zebra", :action => "bird"})
+
+  get("/dice/5/4", {:controller => "zebra", :action => "mouse"})
 end
